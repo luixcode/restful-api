f2e28b43a1a1ebf407a57eea51665901f2f3a836
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-26 14:53:40 -0400

    Enable MVC Framework

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace RESTfulAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,8 @@ namespace RESTfulAPI
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+
+                endpoints.MapControllers();
             });
         }
     }
