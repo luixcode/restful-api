bb2be06bfc1fdd47abbf4bfd6fb01ea90e0ea8c1
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-26 18:47:16 -0400

    Add Custom Attribute

---
+++ CustomAttributes/SexAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using RESTfulAPI.Models;
+
+namespace RESTfulAPI.CustomAttributes
+{
+    public class SexAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var elephant = (ElephantTargetBinding)validationContext.ObjectInstance;
+
+            if (elephant.Sex != "Male" && elephant.Sex != "Female")
+            {
+                return new ValidationResult("Sex should be described as \"Female\" or \"Male\".");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
