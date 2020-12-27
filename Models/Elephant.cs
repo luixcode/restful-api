2ab7789aedd3df13501c90d98a3e82e5c002f3d8
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-26 20:09:42 -0400

    Add Data Models

---
+++ Models/Elephant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTfulAPI.Models
+{
+    public class Elephant
+    {
+        public int Id { get; set; }        
+        public string Name { get; set; }
+        public string Affiliation { get; set; }
+        public string Species { get; set; }
+        public string Sex { get; set; }        
+    }
+}
---
+++ Models/ElephantTargetBinding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using RESTfulAPI.CustomAttributes;
+using RESTfulAPI.Models;
+
+namespace RESTfulAPI.Models
+{
+    public class ElephantTargetBinding
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Affiliation { get; set; }
+        [Required]
+        public string Species { get; set; }
+        [Sex]
+        public string Sex { get; set; }
+
+        public Elephant ToElephant()
+        {
+            return new Elephant
+            {
+                Name = Name,
+                Affiliation = Affiliation,
+                Species = Species,
+                Sex = Sex
+            };
+        }
+    }
+}
