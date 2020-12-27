397c6c73b8b41c4c8d69c7d0c03b640b3a5d07d6
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2020-12-26 20:08:20 -0400

    Enable Json.NET Serializer

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.AspNetCore.Mvc;
+
 namespace RESTfulAPI
 {
     public class Startup
@@ -16,7 +18,7 @@ namespace RESTfulAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,7 +35,7 @@ namespace RESTfulAPI
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("By @luixcode!");
                 });
 
                 endpoints.MapControllers();
